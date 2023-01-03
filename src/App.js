8715fbb0996f3f9ffbaaff0cee5e12aadd4d65e2
Joanna Makarewicz <joanna.milun@gmail.com>
2023-01-03 16:26:59 +0100

    JS functional component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import "./App.css";
 
-function App() {
+import React, { useState } from "react";
+
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useState(() => {
+    setInterval(() => {
+      setSeconds((seconds) => seconds + 1);
+    }, 1000);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
+        <p>
+          This app shows seconds timer - the start moment is when this app is
+          opened.
+        </p>
       </header>
+      <main className="App-main">
+        <h1>Let's count!</h1>
+        <p className="App-counter">{seconds}</p>
+        <a href="index.html" className="App-refresher">
+          Odśwież stronę
+        </a>
+      </main>
     </div>
   );
 }
+
+function App () {
+  return (
+    Timer()
+  )
+};
+
 
 export default App;
